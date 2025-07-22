ede5ebc6fb999622b5b2a583276627cd715570da
Rathan S R <Rathan.SR@MediaKind.com>
2025-07-22 14:04:14 +0530

    Added add function

--- app.py
+++ app.py
@@ -2,3 +2,8 @@ import requests
 print("Hello from Jenkins Python job!")
 response = requests.get("https://httpbin.org/get")
 print("Status Code:", response.status_code)
+
+
+def add(a,b):
+    print(a+b)
+    return a+b
